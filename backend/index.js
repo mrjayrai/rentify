d63d75d8f48e480c5ee42630b9886b80def2d853
mrjayrai <raijai127@gmail.com>
2024-05-19 16:53:58 +0530

    created userschema

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const dbconnect = require('./db/dbconfig');
 const app = express();
 const port = 3000;
-
+const User = require('./db/models/User');
+User();
 // Middleware to parse JSON bodies
 app.use(express.json());
 dbconnect();
