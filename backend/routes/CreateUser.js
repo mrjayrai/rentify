8b93188b9b623d092f8021e044b050a474906eda
mrjayrai <raijai127@gmail.com>
2024-05-19 17:21:31 +0530

    user encryption

--- backend/routes/CreateUser.js
+++ backend/routes/CreateUser.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../db/models/User');
+const crypto = require('crypto-js');
 
 // Route to create a new user
 router.post('/', async (req, res) => {
-  const { name, email, password,usertype } = req.body;
+  const { name, email, password, usertype } = req.body;
   try {
-    const user = new User({ name, email, password,usertype });
+    // Encrypt the password
+    const hashedPassword = crypto.MD5(password).toString();
+
+    const user = new User({ name, email, password: hashedPassword, usertype });
     await user.save();
     res.status(201).json(user);
   } catch (error) {
