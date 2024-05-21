839a89576feebfeac781d687f298f6fd68bacd95
mrjayrai <raijai127@gmail.com>
2024-05-21 07:07:16 +0530

    change

--- backend/db/models/User.js
+++ backend/db/models/User.js
@@ -22,6 +22,10 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  likedProperties: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Property' 
+}]
 });
 
 const User = mongoose.model('User', userSchema);
